99c2a04a8dad455c1075fd673dbb778dd0b43d63
Eric <eric@macbook-white.gateway.2wire.net>
2012-06-21 15:34:01 -0700

    Initial Commit

---
+++ randomNumbers/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  randomNumbers
+//
+//  Created by Eric on 6/21/12.
+//  Copyright (c) 2012 Eric. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
